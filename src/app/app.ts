9e986121432ba1d376172b2950c0ac79f1534794
Luis Miguel <luismy852@gmail.com>
2025-10-25 12:38:03 -0500

    koalli landing page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
+import { Hero } from "./components/hero/hero";
+import { Features } from "./components/features/features";
+import { Testimonies } from "./components/testimonies/testimonies";
+import { Hero2 } from "./components/hero2/hero2";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Hero, Features, Testimonies, Hero2, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
